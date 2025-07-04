dd95d930b9427b12f238d5435fe9f70a77a2a32c
Godisstrut <erikh13255@gmail.com>
2025-07-04 11:41:19 +0200

    Header styling
    Added images and styling for the front-page and header

--- skin-clinic-app/src/components/Header.js
+++ skin-clinic-app/src/components/Header.js
@@ -7,12 +7,10 @@ function Header() {
     return(
         <div className="header">
             <img src={logo} alt="Bild på företagets logga"></img>
-        <nav className="nav">
             <Link to="/">Startsida</Link>
             <Link to="/treatments">Behandlingar</Link>
             <Link to="/trademarks">Varumärken</Link>
             <Link to="/contact">Kontakt oss</Link>
-        </nav>
         </div>
     )
 }
--- skin-clinic-app/src/pages/Home.js
+++ skin-clinic-app/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Home.css"
 
 function Home() {
     return(
-        <div>
+        <div className="home">
             <h1>Startsida</h1>
         </div>
     )
