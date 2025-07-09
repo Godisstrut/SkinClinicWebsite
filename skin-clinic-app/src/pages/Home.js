dd2b40d305c95684632733bfb61ff3afbf8b934e
Godisstrut <erikh13255@gmail.com>
2025-07-09 12:17:55 +0200

    Header color
    Added new pink color for header

--- skin-clinic-app/src/pages/Home.js
+++ skin-clinic-app/src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
     return(
         <div className="home">
             <ImageCarousel />
-            <p>Malmö skinclinic - Storgatan 39A, 211 42, Malmö Boka tid: 0729785925 eller på mail: </p>
+            <p>Malmö skinclinic - Storgatan 39A, 211 42, Malmö Boka tid: +46729785925 eller på mail: malmoskinclinic@gmail.com </p>
             <h2>Din estetiska klinik i centrum av Malmö inom injektionsbehandligar som fillers och botox</h2>
             <h3>Om företaget</h3>
             <p>Efter mer än 10 år i branschen är Malmö skin clinic äntligen på boka direkt. Familjeföretaget som drivs av mor och son med erfarenheter som legitimerade sjuksköterskor, främst inom estetik!  </p>
