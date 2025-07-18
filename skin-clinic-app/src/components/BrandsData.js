4a1bb015fa242834cb13f042fc653a3b6ca08eef
Godisstrut <erikh13255@gmail.com>
2025-07-18 11:03:02 +0200

    Trademarks update
    Updated the view of brands in the trademarks tab, also adjusted carousel intervals

--- skin-clinic-app/src/components/BrandsData.js
+++ skin-clinic-app/src/components/BrandsData.js
@@ -1,4 +1,5 @@
 import BringOutBestMolderm from "../img/BringOutBestMolderm.png"
+import YouthfulMoisture from "../img/YouthfulMoisture.png"
 
 const brands = [
     {
@@ -6,6 +7,11 @@ const brands = [
         image: BringOutBestMolderm,
         description: "WE BELIEVE IN NATURAL BEAUTY! Natural beauty goes deeper than what you can see on the surface.- How does the gel act within the tissue? How does it integrate? How pure is it? How evenly does it break down?There are a lot of questions that need to be asked when choosing a dermal filler. With Molderm® you get natural beauty from the inside and out."
     },
+    {
+        name: "Molderm",
+        image: YouthfulMoisture ,
+        description: "Molderm® GLOW - Bringing youthful moisture to your skin."
+    }
 ]
 
 export default brands
