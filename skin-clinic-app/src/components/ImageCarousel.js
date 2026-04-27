9625049672f6fefdce3807ca701abf7dffd10b7b
Godisstrut <erikh13255@gmail.com>
2026-04-27 10:17:49 +0200

    Small update
    Update boxshadow on image carousel

--- skin-clinic-app/src/components/ImageCarousel.js
+++ skin-clinic-app/src/components/ImageCarousel.js
@@ -2,10 +2,11 @@ import Carousel from 'react-bootstrap/Carousel';
 import Denni_new from "../img/Denni_new.jpg"
 import skinclinic_gata from "../img/skinclinic_gata.jpg"
 import skinclinic_op from "../img/skinclinic_op.jpg";
+import "./ImageCarousel.css";
 
 function ImageCarousel() {
   return (
-    <div style={{maxWidth: "50em", margin: "0 auto"}} >
+    <div className='image-carousel' style={{maxWidth: "50em", margin: "0 auto"}} >
     <Carousel>
       <Carousel.Item interval={4000}>
         <img className='d-block w-100' src={skinclinic_gata} alt="Bild på utanför skinclinic" style={{ height: '60em', objectFit: 'cover' }} >
